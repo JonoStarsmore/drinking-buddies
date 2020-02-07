ac174201fb9d2201ab4386ef69e9cb8129f6a5c7
JonoStarsmore <jonathanejaworski@gmail.com>
2020-02-06 19:31:18 -0600

    addEvent form

--- src/addEvent.js
+++ src/addEvent.js
@@ -1,3 +1,62 @@
+import React, { Component } from 'react';
+import APIContext from './APIContext';
+
+
+export default class AddEvent extends Component {
+
+    state = {
+        title:'',
+        location: '',
+        description:'',
+    }
+
+    onSubmitForm = (e) => {
+        e.preventDefault();
+
+        // get all input data from state 
+
+        // submit that information where it needs to go
+
+    }
+
+    onInputChange = e => {
+        // determine which input was changed so we can reuse this function for all inputs
+        const inputType = e.target.name;
+
+        // update the state for that input with the new value
+        this.setState({
+            [inputType]: e.target.value
+        })
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.onSubmitForm}>
+            <fieldset>
+                <legend>Add Event</legend>
+                <label htmlFor='title'>
+                    title:
+                    <input onChange={this.onInputChange} className='input-title' name='title' type="text" />
+                </label>
+                <label htmlFor='location'>
+                        location:
+                    <input className='input-location' name='location'type="text" />
+                </label>
+                <label htmlFor="description">
+                        description:
+                    <input className='input-description' name='description' type="text" />
+                </label>
+                <label htmlFor="date">
+                        date:
+                    <input className='input-date' name='date' type="date" />
+                </label>
+            </fieldset>
+            </form>
+        )
+    }
+} 
+
+
 /*1. add event component -> post to data. API.
     - Add event form
     - create post request that adds submission to data
